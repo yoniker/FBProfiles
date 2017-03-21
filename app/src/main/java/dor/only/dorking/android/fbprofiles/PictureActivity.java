5a13e21aff58490b2fde2c18180d718d796a158b
Yoni Keren <yoni.keren@yahoo.ca>
2017-03-21 16:56:14 +0200

    support downloading multiple names, all pics per name

--- app/src/main/java/dor/only/dorking/android/fbprofiles/PictureActivity.java
+++ app/src/main/java/dor/only/dorking/android/fbprofiles/PictureActivity.java
@@ -2,9 +2,12 @@ package dor.only.dorking.android.fbprofiles;
 
 import android.app.DownloadManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,18 +28,26 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import static com.facebook.AccessToken.getCurrentAccessToken;
+import static dor.only.dorking.android.fbprofiles.SearchScreen.SHARED_PREF_NAMES_KEY;
 
 public class PictureActivity extends AppCompatActivity {
     //The key string we will use to get the parceable listarray of id strings.
-    public static final String IDS_DATA_KEY="the key :)";
+    public static final String IDS_DATA_KEY="IDS_KEY";
+    public static final String NAME_PERSON_KEY="NAME_PERSON_KEY";
 
     private ArrayList<String> ids;
     private TextView personStatsTV;
     private ImageView personImage;
-    private Button downloadPicture;
+    private Button downloadPictures;
+    private TextView downloadStatusTV;
     private Button showNextPersonButton;
+    private String nameOfPerson;
     private String thePicUrl;
     Random rn = new Random();
+
+
+    //I am not familiar with a better way,so the FB graph callbacks will communicate through the activity's variables
+    private Integer currentDownloadIndex;
 
     //Some string constants related to a picture related to a picture
 
@@ -50,11 +61,13 @@ public class PictureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
         ParceableIds theIds=getIntent().getParcelableExtra(IDS_DATA_KEY);
+        nameOfPerson=getIntent().getStringExtra(NAME_PERSON_KEY);
         ids=theIds.getTheList();
         personStatsTV=(TextView)findViewById(R.id.personStatsText);
         personImage=(ImageView)findViewById(R.id.theImage);
         showNextPersonButton=(Button)findViewById(R.id.showNextPerson);
-        downloadPicture=(Button) findViewById(R.id.downloadPicture);
+        downloadPictures=(Button) findViewById(R.id.downloadPictures);
+        downloadStatusTV=(TextView)findViewById(R.id.downloadStatus);
         showSomeoneRandom();
         showNextPersonButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -67,22 +80,31 @@ public class PictureActivity extends AppCompatActivity {
 
         );
 
-        downloadPicture.setOnClickListener(
+        downloadPictures.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        downloadThePicture();
+                        downloadAllPictures();
                     }
                 }
 
         );
 
+        downloadAllPictures();
 
+    }
+
+    private void downloadAllPictures(){
+        showNextPersonButton.setVisibility(View.GONE);
+        downloadPictures.setVisibility(View.GONE);
+        downloadStatusTV.setVisibility(View.VISIBLE);
+        currentDownloadIndex=0;
+        downloadCurrentPersonByIndex();
 
     }
 
 
-    private class myCallBack implements GraphRequest.Callback{
+    private class CallbackForRandomPic implements GraphRequest.Callback{
         @Override
         public void onCompleted(GraphResponse response) {
             //Load the picture
@@ -93,7 +115,7 @@ public class PictureActivity extends AppCompatActivity {
                     thePicUrl=picData.getString(FB_URL);
 
                     Picasso.with(getApplicationContext()).load(thePicUrl).into(personImage);
-                    downloadPicture.setEnabled(true);
+                    downloadPictures.setEnabled(true);
 
                 } else{
                     personStatsTV.setText(personStatsTV.getText()+" User has no profile Pic!");
@@ -108,27 +130,90 @@ public class PictureActivity extends AppCompatActivity {
         }
     }
 
-
-
-
     private void showSomeoneRandom(){
         int theUserChosen=rn.nextInt(ids.size());
         String theId=ids.get(theUserChosen);
-        downloadPicture.setEnabled(false);
         personStatsTV.setText(theId+" was chosen, index is:"+theUserChosen+" out of "+ids.size());
         //Now look for the person's pic path
         AccessToken theUserToken=getCurrentAccessToken();
         Bundle params = new Bundle();
         params.putBoolean("redirect", false);
         GraphRequest searchRequest=new GraphRequest(theUserToken,theId+"/picture?type=square&height=200",params, HttpMethod.GET);
-        searchRequest.setCallback(new PictureActivity.myCallBack());
+        searchRequest.setCallback(new PictureActivity.CallbackForRandomPic());
         searchRequest.executeAsync();
 
 
 
     }
 
-    private void downloadThePicture(){
+    private class CallbackForDownloading implements GraphRequest.Callback{
+        @Override
+        public void onCompleted(GraphResponse response) {
+            //Load the picture
+            JSONObject picData=response.getJSONObject();
+            try {
+                picData=picData.getJSONObject(FB_DATA);
+                if (!picData.getBoolean(FB_SILHOUETTE)) {
+                    thePicUrl=picData.getString(FB_URL);
+
+                    Picasso.with(getApplicationContext()).load(thePicUrl).into(personImage);
+                    downloadThePicture(thePicUrl,ids.get(currentDownloadIndex),currentDownloadIndex.toString());
+
+                } else{
+                    //personStatsTV.setText(personStatsTV.getText()+" User has no profile Pic!");
+
+                }
+                currentDownloadIndex+=1;
+                downloadCurrentPersonByIndex();
+
+            }
+            catch(Throwable e){
+                Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG);
+
+            }
+        }
+    }
+
+
+
+
+
+    public  void removeFirstNameFromSharedPref(){
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String allNames=sharedPref.getString(SHARED_PREF_NAMES_KEY,"");
+        allNames=allNames.trim();
+        String firstName=allNames.split(" ")[0];
+        int firstNameLength=firstName.length();
+        allNames=allNames.substring(firstNameLength);
+        allNames=allNames.trim();
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(SHARED_PREF_NAMES_KEY, allNames);
+        editor.commit();
+
+    }
+
+
+    void downloadCurrentPersonByIndex(){
+        int theUserChosen=currentDownloadIndex;
+        if(theUserChosen>=ids.size()) {
+            removeFirstNameFromSharedPref();
+            Intent searchScreen=new Intent(this,SearchScreen.class);
+            startActivity(searchScreen);
+            return;}
+        String theId=ids.get(theUserChosen);
+        downloadStatusTV.setText(theId+" Queuing "+theUserChosen+" out of "+ids.size()+" from:"+nameOfPerson);
+        //Now look for the person's pic path
+        AccessToken theUserToken=getCurrentAccessToken();
+        Bundle params = new Bundle();
+        params.putBoolean("redirect", false);
+        GraphRequest searchRequest=new GraphRequest(theUserToken,theId+"/picture?type=square&height=200",params, HttpMethod.GET);
+        searchRequest.setCallback(new PictureActivity.CallbackForDownloading());
+        searchRequest.executeAsync();
+
+    }
+
+    private void downloadThePicture(String thePicUrl,String thePersonsId,String thePersonsIndex){
         if(thePicUrl==null || thePicUrl.equals("")){
             return;
         }
@@ -142,7 +227,6 @@ public class PictureActivity extends AppCompatActivity {
         }
 
         DownloadManager mgr = (DownloadManager)this.getSystemService(Context.DOWNLOAD_SERVICE);
-
         Uri downloadUri = Uri.parse(thePicUrl);
         DownloadManager.Request request = new DownloadManager.Request(
                 downloadUri);
@@ -152,7 +236,7 @@ public class PictureActivity extends AppCompatActivity {
                         | DownloadManager.Request.NETWORK_MOBILE)
                 .setAllowedOverRoaming(true).setTitle("Demo")
                 .setDescription("Something useful. No, really.")
-                .setDestinationInExternalPublicDir("/DorDownloadToHere", "DorfileName.jpg");
+                .setDestinationInExternalPublicDir("/DorDownloadToHere", nameOfPerson+"_"+thePersonsIndex+"_"+thePersonsId+".jpg");
 
         mgr.enqueue(request);
 
--- app/src/main/java/dor/only/dorking/android/fbprofiles/SearchScreen.java
+++ app/src/main/java/dor/only/dorking/android/fbprofiles/SearchScreen.java
@@ -1,8 +1,10 @@
 package dor.only.dorking.android.fbprofiles;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -39,9 +41,10 @@ import static com.facebook.AccessToken.getCurrentAccessToken;
 public class SearchScreen extends AppCompatActivity {
 
     //The views which the user can see
-    private EditText theNameToSearch;
+    private EditText theNamesToSearchEditText;
     private NumberPicker numberOfPeople;
     private Button goSearchButton;
+    private Button saveToSharedPrefButton;
     private TextView progressMadeTV;
 
 
@@ -49,6 +52,8 @@ public class SearchScreen extends AppCompatActivity {
     private ArrayList<String> firstNames;
     Integer namesGotSoFar=new Integer(0);
     private boolean doneFillingIds=false;
+
+    public static final String SHARED_PREF_NAMES_KEY="the_names";
 
     //Some String constants given the current Facebook graph API (2.8 as of 24-Oct-2016).
     private static final String FB_DATA="data";
@@ -155,7 +160,7 @@ public class SearchScreen extends AppCompatActivity {
     //Removes all of the people without the first name being the same as the one we want.
     private void siftByFirstName(){
         ArrayList<String> idsWithRelevantFirstName=new ArrayList<>();
-        String theNameWeWant=theNameToSearch.getText().toString();
+        String theNameWeWant=getNameToSearch();
         for(int i=0; i<ids.size(); ++i){
             if(firstNames.get(i).equalsIgnoreCase(theNameWeWant)){
                 idsWithRelevantFirstName.add(ids.get(i));
@@ -176,6 +181,8 @@ public class SearchScreen extends AppCompatActivity {
         Intent downloadPicture=new Intent(this,PictureActivity.class);
         ParceableIds theIds=new ParceableIds(ids);
         downloadPicture.putExtra(PictureActivity.IDS_DATA_KEY,theIds);
+        String theNameWeWant=getNameToSearch();
+        downloadPicture.putExtra(PictureActivity.NAME_PERSON_KEY,theNameWeWant);
         startActivity(downloadPicture);
 
 
@@ -247,6 +254,20 @@ public class SearchScreen extends AppCompatActivity {
     }
 
 
+    private String getNameToSearch(){
+        SharedPreferences sharedPref =  PreferenceManager.getDefaultSharedPreferences(this);
+        String allNames = sharedPref.getString(SHARED_PREF_NAMES_KEY, "");
+        allNames=allNames.trim();
+        if(allNames.equals("")){
+            return "";
+
+        }
+
+        return allNames.split(" ")[0];
+
+    }
+
+
 
 
     private void readProfilesPage(final String thePageAddress, final int numberOfIdsLeft){
@@ -286,27 +307,65 @@ public class SearchScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
-        theNameToSearch=(EditText)findViewById(R.id.theNameToSearch);
+        theNamesToSearchEditText=(EditText)findViewById(R.id.theNamesToSearch);
         numberOfPeople=(NumberPicker)findViewById(R.id.numberOfPeople);
         goSearchButton=(Button)findViewById(R.id.goSearchButton);
+        saveToSharedPrefButton=(Button)findViewById(R.id.saveNamesToSharedPreferences);
         progressMadeTV=(TextView)findViewById(R.id.progressMadeTV);
+
 
 
         numberOfPeople.setMaxValue(1000);
         numberOfPeople.setMinValue(1);
+        numberOfPeople.setValue(1000);
         theUserToken=getCurrentAccessToken();
+
+
+        saveToSharedPrefButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String namesEntered=theNamesToSearchEditText.getText().toString();
+                        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        editor.putString(SHARED_PREF_NAMES_KEY, namesEntered);
+                        editor.commit();
+
+                    }}
+            );
 
         goSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String theName=theNameToSearch.getText().toString();
-                GraphRequest searchRequest=new GraphRequest(theUserToken,"search?q="+'"'+theName+'"'+"&type=user");
-                searchRequest.setCallback(new FirstCallback());
-                searchRequest.executeAsync();
+                startSearch();
             }
         });
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);;
+        String allNames = sharedPref.getString(SHARED_PREF_NAMES_KEY, "");
+        theNamesToSearchEditText.setText(allNames);
+        if(allNames.trim().length()>0){
+            if(allNames.equals("LetsGo")){
+                allNames="Noah Liam Mason Jacob William Ethan James Alexander Michael Benjamin Elijah Daniel Aiden Logan Matthew Lucas Jackson David Oliver Jayden Joseph Gabriel Samuel Carter Anthony John Dylan Luke Henry Andrew Isaac Christopher Joshua Wyatt Sebastian Owen Caleb Nathan Ryan Jack Hunter Levi Christian Jaxon Julian Landon Grayson Jonathan Isaiah Charles Thomas Aaron Eli Connor Jeremiah Cameron Josiah Adrian Colton Jordan Brayden Nicholas Robert Angel Hudson Lincoln Evan Dominic Austin Gavin Nolan Parker Adam Chase Jace Ian Cooper Easton Kevin Jose Tyler Brandon Asher Jaxson Mateo Jason Ayden Zachary Carson Xavier Leo Ezra Bentley Sawyer Kayden Blake Nathaniel Ryder Theodore Elias Tristan Roman Leonardo Camden Brody Luis Miles Micah Vincent Justin Greyson Declan Maxwell Juan Cole Damian Carlos Max Harrison Weston Brantley Braxton Axel Diego Abel Wesley Santiago Jesus Silas Giovanni Bryce Jayce Bryson Alex Everett George Eric Ivan Emmett Kaiden Ashton Kingston Jonah Jameson Kai Maddox Timothy Ezekiel Ryker Emmanuel Hayden Antonio Bennett Steven Richard Jude Luca Edward Joel Victor Miguel Malachi King Patrick Kaleb Bryan Alan Marcus Preston Abraham Calvin Colin Bradley Jeremy Kyle Graham Grant Jesse Kaden Alejandro Oscar Jase";
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(SHARED_PREF_NAMES_KEY, allNames);
+                editor.commit();
+
+            }
+            startSearch();
+
+        }
 
 
+
+
+    }
+    private void startSearch(){
+        String theName=getNameToSearch();
+        theName=theName.trim();
+        if(theName.equals("")){return;}
+        theNamesToSearchEditText.setEnabled(false);
+        GraphRequest searchRequest=new GraphRequest(theUserToken,"search?q="+'"'+theName+'"'+"&type=user");
+        searchRequest.setCallback(new FirstCallback());
+        searchRequest.executeAsync();
 
     }
 }
